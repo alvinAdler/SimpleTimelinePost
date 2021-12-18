0a9049134d6124a3c5fb2d91f32198df47039242
alvinAdler <alvinadlersampow@gmail.com>
2021-12-18 18:43:54 +0700

    Initialized server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config()
+
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on("error", (err) => console.log(err))
+db.once("open", () => console.log("Connected to Database"))
+
+app.listen("5000", () => {
+    console.log("Server Started")
+})
