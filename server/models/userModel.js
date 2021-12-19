6cd76974ea737b203e7f148e9d43f342bb840b93
alvinAdler <alvinadlersampow@gmail.com>
2021-12-20 02:49:47 +0700

    Added login functionality and added extra validations in the registration API
    The login functionality have not utilized jwt token

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -14,6 +14,11 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    friendsList: {
+        type: [mongoose.ObjectId],
+        required: true,
+        default: []
     }
 })
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const bcrypt = require("bcrypt")
 const router = express.Router()
 
 const userModel = require("../models/userModel")
@@ -9,20 +10,55 @@ router.get("/all", (req, res) => {
     })
 })
 
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
-    userModel.create({username, password}, (err, doc) => {
+    const result = await userModel.findOne({username: username})
+
+    if(result){
+        return res.status(400).json({
+            message: "User already exist"
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    userModel.create({username, password: hashedPassword}, (err, doc) => {
         if(err){
             return res.status(500).json({
-                message: "Failed to create user"
+                message: "Failed to create user."
             })
         }
 
         return res.status(201).json({
             message: `User ${doc.username} has been successfully created`
         })
+    })
+})
+
+router.post("/login", async (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    const fetchedUser = await userModel.findOne({username: username})
+
+    if(!fetchedUser){
+        return res.status(400).json({
+            message: "Username does not exist. Please recheck your login credentials."
+        })
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, fetchedUser.password)
+
+    if(!isPasswordMatch){
+        return res.status(400).json({
+            message: "Password does not match. Please recheck your password."
+        })
+    }
+
+    return res.status(200).json({
+        message: "Access granted."
     })
 })
 
