06920128d212c0d82888b4370e1bd1c974899f25
alvinAdler <alvinadlersampow@gmail.com>
2021-12-27 17:10:26 +0700

    Styled the login page
    Not responsive yet

--- client/src/components/LoginPage/LoginPage.jsx
+++ client/src/components/LoginPage/LoginPage.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import styles from './LoginPage_master.module.css'
 
-import PlaceholderJumbotron from '../utilityComponents/PlaceholderJumbotron/PlaceholderJumbotron'
-
 const LoginPage = () => {
     return (
-        <div>
-            <PlaceholderJumbotron title="Login Page"/>
+        <div className={styles.loginPage_container}>
+            <div className={styles.welcomingBanner}>
+                <h1>Enter our <span className="text-emphasize">world</span></h1>
+                <img className={styles.imageBanner} src="/images/undraw_login_re_4vu2.svg" alt="Welcome" />
+            </div>
+            <form className={styles.loginForm}>
+                <h2>Login</h2>
+                <input className={styles.loginFormInputs} type="text" placeholder="Username"/>
+                <input className={styles.loginFormInputs} type="password" placeholder="Password"/>
+                <button className={styles.loginSubmitButton} type="submit">Login</button>
+                <p className={styles.registrationLink}>Doesn't have an account? <Link to="/register">Register here</Link></p>
+            </form>
         </div>
     )
 }
