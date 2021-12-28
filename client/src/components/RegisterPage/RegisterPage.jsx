e00fd922b8950fda5c83de1ff1e2be0a8b0bce0a
alvinAdler <alvinadlersampow@gmail.com>
2021-12-28 16:01:08 +0700

    Styled the registration page

--- client/src/components/RegisterPage/RegisterPage.jsx
+++ client/src/components/RegisterPage/RegisterPage.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 import styles from "./RegisterPage_master.module.css"
 
-import PlaceholderJumbotron from '../utilityComponents/PlaceholderJumbotron/PlaceholderJumbotron'
-
 const RegisterPage = () => {
     return (
-        <div>
-            <PlaceholderJumbotron title="Register Page"/>
+        <div className={styles.registerPageContainer}>
+            <form className={styles.registerForm}>
+                <h2>Register</h2>
+                <input className={styles.registerFormInputs} type="text" placeholder="Username"/>
+                <input className={styles.registerFormInputs} type="password" placeholder="Password"/>
+                <input className={styles.registerFormInputs} type="password" placeholder="Confirm Password"/>
+                <button className={styles.registerSubmitButton} type="submit">Register</button>
+            </form>
+            <div className={styles.welcomingBanner}>
+                <h1>Unite the <span className="text-emphasize">people</span></h1>
+                <img className={styles.imageBanner} src="/images/undraw_certification_aif8.svg" alt="Register" />
+            </div>
         </div>
     )
 }
