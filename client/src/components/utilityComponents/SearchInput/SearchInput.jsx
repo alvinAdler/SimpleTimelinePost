1099b2ef9d65b4a3dbfa24871a8b2d0025b1e113
alvinAdler <alvinadlersampow@gmail.com>
2022-02-20 21:47:39 +0700

    Added a reset button in the SearchInput component

--- client/src/components/utilityComponents/SearchInput/SearchInput.jsx
+++ client/src/components/utilityComponents/SearchInput/SearchInput.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import { FaSearch } from 'react-icons/fa'
+import { useState, useRef } from 'react'
+import { FaSearch, FaTimes } from 'react-icons/fa'
 
 import './SearchInput_master.css'
 
@@ -8,13 +8,23 @@ import BaseButton from '../Buttons/BaseButton/BaseButton'
 const SearchInput = ({mainClass, inputClass, placeholderText="Search here", searchCallback, ...rest}) => {
 
     const [searchInput, setSearchInput] = useState("")
+    
+    const searchInputRef = useRef(null)
+
+    const handleResetSearch = () => {
+        setSearchInput("")
+        searchInputRef.current.focus()
+    }
 
     return (
         <div className={`${mainClass} search-input-container`}>
             <input {...rest} className={`${inputClass}`}
             placeholder={placeholderText}
+            value={searchInput}
             onChange={(ev) => setSearchInput(ev.target.value)}
+            ref={searchInputRef}
             />
+            {searchInput !== "" && <button className="reset-search-button" onClick={handleResetSearch}><FaTimes/></button>}
             <BaseButton buttonText={<FaSearch/>} onClick={() => searchCallback(searchInput)}/>
         </div>
     )
